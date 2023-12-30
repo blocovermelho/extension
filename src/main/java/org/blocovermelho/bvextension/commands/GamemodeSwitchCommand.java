a916207c283060119ddc4cb0b8c8bd34b393f9ed
Alice Isabel <roridev@protonmail.com>
2023-12-30 16:59:23 -0300

    Use minimessage instead of vanilla MutableText

--- src/main/java/org/blocovermelho/bvextension/commands/GamemodeSwitchCommand.java
+++ src/main/java/org/blocovermelho/bvextension/commands/GamemodeSwitchCommand.java
@@ -1,21 +1,15 @@
 package org.blocovermelho.bvextension.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.network.packet.s2c.play.PositionFlag;
-import net.minecraft.registry.RegistryKey;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import oshi.util.tuples.Pair;
+import org.blocovermelho.bvextension.Extension;
+import org.blocovermelho.bvextension.types.LastRecordedPosition;
 
 import java.util.HashMap;
-import java.util.Set;
 import java.util.UUID;
 
 import static net.minecraft.server.command.CommandManager.literal;
@@ -27,6 +21,9 @@ public class GamemodeSwitchCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("s").executes(c -> {
             var source = (ServerCommandSource) c.getSource();
+            var audience = Extension.audiences.audience(source);
+            Sound sound = Sound.sound(Key.key("block.amethyst_block.chime"), Sound.Source.PLAYER, 0.4f, 1);
+
             if (!source.isExecutedByPlayer()) {
                 return 1;
             }
@@ -39,8 +36,9 @@ public class GamemodeSwitchCommand {
                         player.getWorld().getRegistryKey(),
                         player.getYaw(), player.getPitch()));
                 player.changeGameMode(GameMode.SPECTATOR);
-                player.sendMessage(Text.literal("[/s] SURVIVAL -> SPECTATOR"), true);
-                player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.PLAYERS, 0.4f, 1);
+
+                audience.sendActionBar(Extension.MM.deserialize("<yellow>[/s] <red>SURVIVAL <white>-> <green>SPECTATOR"));
+                audience.playSound(sound);
             } else if (gamemode == GameMode.SPECTATOR) {
                 var pos = oldPos.remove(player.getUuid());
 
@@ -51,11 +49,11 @@ public class GamemodeSwitchCommand {
                 player.teleport(world, pos.pos().getX(), pos.pos().getY(), pos.pos().getZ(), pos.yaw(), pos.pitch());
 
                 player.changeGameMode(GameMode.SURVIVAL);
-                player.sendMessage(Text.literal("[/s] SPECTATOR -> SURVIVAL"), true);
-                player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.PLAYERS, 0.4f, 1);
+                audience.sendActionBar(Extension.MM.deserialize("<yellow>[/s] <red>SPECTATOR <white>-> <green>SURVIVAL"));
+                audience.playSound(sound);
 
             } else {
-                player.sendMessage(Text.literal("[/s] Current gamemode not supported by gamemode [/s]witcher."));
+                audience.sendMessage(Component.text("[/s] Current gamemode not supported by gamemode [/s]witcher."));
             }
 
             return 1;
