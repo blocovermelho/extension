5788b6522d1ca4bbd036039a26b7266140f4791d
alikindsys <alice@blocovermelho.org>
2024-02-29 00:51:30 -0300

    Fix BlockEntityTag being applied to shulker boxes
    Closes #5

--- src/main/java/org/blocovermelho/bvextension/utils/Inventory.java
+++ src/main/java/org/blocovermelho/bvextension/utils/Inventory.java
@@ -2,6 +2,7 @@ package org.blocovermelho.bvextension.utils;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -23,6 +24,10 @@ public class Inventory {
             getDroppedStacks(state, sworld, pos ,blockEntity, entity, stack).forEach( is -> {
                 var item = is.getItem();
                 var count = is.getCount();
+
+                if (blockEntity instanceof ShulkerBoxBlockEntity sbbe) {
+                    is.removeSubNbt("BlockEntityTag");
+                }
 
                 if (entity.getInventory().insertStack(is)) {
                     sworld.playSound(null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.1f, (sworld.random.nextFloat() - sworld.random.nextFloat()) * 1);
